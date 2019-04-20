89f27837c38eeb08835d5c1af75cd30bc1aff57e
gnzandrs <gavr@outlook.cl>
2019-04-20 19:31:58 -0400

    metodos CRUD

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -24,5 +24,56 @@ namespace DotNetApi.Controllers
         {
             return await _context.Usuario.Take(10).ToListAsync();
         }
+
+        // GET: api/usuario/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+
+            if (usuario == null)
+                return NotFound();
+
+            return usuario;
+        }
+
+        // POST: api/usuario
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> AddUsuario(Usuario usuario)
+        {
+            _context.Usuario.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUsuario), new { id = usuario.IdUsuario }, usuario);
+        }
+
+        // PUT: api/usuario/2
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUsuario(int id, Usuario usuario)
+        {
+            if (id != usuario.IdUsuario)
+                return BadRequest();
+
+            _context.Entry(usuario).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/usuario/2
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUsuario(int id)
+        {
+            var usuario = await _context.Usuario.FindAsync(id);
+
+            if (usuario == null)
+                return NotFound();
+
+            _context.Usuario.Remove(usuario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
     }
 }
